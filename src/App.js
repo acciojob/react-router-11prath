fbe884b013744d3475bd7b925c1d770822febdfc
PrathviShetty <108prathvi.shetty@gamil.com>
2026-03-18 17:17:53 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Navigation from "./Navigation";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
